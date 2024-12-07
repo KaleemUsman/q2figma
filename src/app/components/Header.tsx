079da6be264e1ffadf765fe368cdd9564f62dba3
KaleemUsman <kaleem927@gmail.com>
2024-12-07 23:58:51 +0500

    first commit

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='header'>
+      <div>
+    home sign in
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from './components/Header'
+
+const page = () => {
+  return (
+ 
+    <div className='top-header'>
+      Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+<button><b>ShopNow</b></button>
+<div className='flex gap-2 text-wrap'>English</div>
+
+  
+    </div>
+    
+ )
+ 
+}
+
+export default page
